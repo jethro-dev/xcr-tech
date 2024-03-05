18fff4e37a71cc7c4a9c89947eb94a758bc1bfbb
Jethro Au <galongau@gmail.com>
2024-03-05 23:01:25 +0000

    update card

--- components/cards-section.tsx
+++ components/cards-section.tsx
@@ -1,12 +1,15 @@
+import Image from "next/image";
 import React from "react";
 
 type Props = {};
 
 const cards = [
-  { img: "", title: "Features" },
-  { img: "", title: "Template gallery" },
-  { img: "", title: "Productivity quiz" },
-  { img: "", title: "Extension gallery" },
+  { img: "/images/augmented-reality.png", title: "Argumented Reality" },
+  { img: "/images/virtual-tour.png", title: "Virtual Reality" },
+  {
+    img: "/images/artificial-intelligence.png",
+    title: "Artificial Intelligence",
+  },
 ];
 
 export const CardsSection = (props: Props) => {
@@ -16,12 +19,15 @@ export const CardsSection = (props: Props) => {
         <h2 className="text-4xl font-bold text-center">
           Explore all XCR has to offer
         </h2>
-        <div className="mt-16 grid grid-cols-2 md:grid-cols-4 gap-4 lg:gap-4 max-w-6xl mx-auto">
+        <div className="mt-16 grid grid-cols-2 md:grid-cols-3 gap-4 lg:gap-4 max-w-6xl mx-auto">
           {cards.map((card, i) => (
             <div
               key={i}
-              className="aspect-square rounded-lg border shadow-sm"
-            ></div>
+              className="aspect-square rounded-lg border shadow-sm flex flex-col items-center justify-center gap-6 bg-white hover:bg-black/[.05] cursor-pointer transition"
+            >
+              <Image src={card.img} alt={card.title} width={50} height={50} />
+              <p>{card.title}</p>
+            </div>
           ))}
         </div>
       </div>
